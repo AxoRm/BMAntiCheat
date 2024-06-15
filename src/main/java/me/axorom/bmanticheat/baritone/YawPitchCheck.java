19eb33f991c8c608e6a762c2cfae0f68b9fc0745
AxoRm <raksenov50@gmail.com>
2024-06-16 02:06:14 +0300

    repair YawPitchCheck

--- src/main/java/me/axorom/bmanticheat/baritone/YawPitchCheck.java
+++ src/main/java/me/axorom/bmanticheat/baritone/YawPitchCheck.java
@@ -7,6 +7,6 @@ public class YawPitchCheck {
     public static boolean check(Location from, Location to) {
         int yawChanged = Float.compare(from.getYaw(), to.getYaw());
         int pitchChanged = Float.compare(from.getPitch(), to.getPitch());
-        return pitchChanged == 0 && yawChanged != 0 && to.getPitch() != 90f && from.getPitch() != 90f;
+        return pitchChanged == 0 && yawChanged != 0 && to.getPitch() != 90f && from.getPitch() != 90f && to.getPitch() != -90f && from.getPitch() != -90f;
     }
 }
